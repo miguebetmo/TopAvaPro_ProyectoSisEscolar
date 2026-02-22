7472ef56bceb24c7b2f4baa710c0e443f43d364b
MIguel Betancourt <miguebetmo@gmail.com>
2026-02-22 10:29:50 -0600

    Update ControladorLogin.java

--- src/itz/controlador/ControladorLogin.java
+++ src/itz/controlador/ControladorLogin.java
@@ -1,9 +1,34 @@
 package itz.controlador;
 
-/**
- *
- * @author marco
- */
+import itz.modelo.Usuario;
+import itz.vista.VentanaLogin;
+
 public class ControladorLogin {
-    
+
+    private Usuario modeloUsuario;
+    private VentanaLogin vistaLogin;
+
+    public ControladorLogin(Usuario modeloUsuario, VentanaLogin vistaLogin) {
+        this.modeloUsuario = modeloUsuario;
+        this.vistaLogin = vistaLogin;
+    }
+
+    public void iniciarSesion() {
+
+        String usuario = vistaLogin.obtenerUsuario();
+        String password = vistaLogin.obtenerPassword();
+
+        boolean valido = validarCredenciales(usuario, password);
+
+        if (valido) {
+            vistaLogin.mostrarMensaje("Inicio de sesión exitoso.");
+        } else {
+            vistaLogin.mostrarMensaje("Usuario o contraseña incorrectos.");
+        }
+    }
+
+    public boolean validarCredenciales(String usuario, String password) {
+
+        return modeloUsuario.verificarCredenciales(usuario, password);
+    }
 }
